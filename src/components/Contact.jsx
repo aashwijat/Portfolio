5be07aff930a111e67a9acc58bc9cc9210751688
Aashwija Thantry <aashwija.thantry@gmail.com>
2025-07-13 18:53:56 +0530

    Change Fonts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,26 +4,28 @@ function Contact() {
     <>
       <Card>
         <div className="text-center">
-          <div className="playfair-card">
-            <span className="block text-[#107373]/50 text-3xl font-bold pt-12">
+          <div className="polaroid-name">
+            <span className="block text-[#107373]/90 text-3xl font-bold pt-12">
               Let's Connect!
             </span>
-            <span className="block text-xl font-bold text-[#107373]/80">
-              <a
-                href="mailto:aashwijathantry613@gmail.com"
-                className="!text-[#107373]/80 !hover:text-gray-800 !visited:text-[#107373]"
-              >
-                mail{" "}
-              </a>
-              |
-              <a
-                href="https://www.linkedin.com/in/aashwijat/"
-                className="!text-[#107373]/80 !hover:text-gray-800 !visited:text-[#107373]"
-              >
-                {" "}
-                linkedin
-              </a>
-            </span>
+            <div className="playfair-card">
+              <span className="block text-xl font-bold text-[#107373]/80">
+                <a
+                  href="mailto:aashwijathantry613@gmail.com"
+                  className="!text-[#107373]/80 !hover:text-gray-800 !visited:text-[#107373]"
+                >
+                  mail{" "}
+                </a>
+                |
+                <a
+                  href="https://www.linkedin.com/in/aashwijat/"
+                  className="!text-[#107373]/80 !hover:text-gray-800 !visited:text-[#107373]"
+                >
+                  {" "}
+                  linkedin
+                </a>
+              </span>
+            </div>
           </div>
         </div>
       </Card>
